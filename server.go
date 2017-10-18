fb575e3454d66596b3edbd5225196cd1f8d53eba
aruna <aruna@sultanyoga.com>
2017-10-18 17:17:29 +0000

    Implement the minimal server and add compile prod script

--- server.go
+++ server.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Welcome to my world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Howdy, mam!")
+	})
+	log.Fatal(http.ListenAndServe(":1025", nil))
 }
